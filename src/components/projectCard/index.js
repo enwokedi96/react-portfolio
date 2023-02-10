914cac4e9ef061745486d8fd996a8834abdf2bae
ezechukwu nwokedi <israellambert96@gmail.com>
2023-02-10 02:23:23 +0000

    correct image paths, removed duplicates, added css

--- src/components/projectCard/index.js
+++ src/components/projectCard/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom"
+import "./style.css";
 
 // <Link to={`/project/${props.id}`}>
 
 function ProjectCard(props){
     return (      
-        <div className="card">
-            <div className="img-container">
+        <div className="card card-image p-2" style={{ backgroundImage: `url(${props.image})`}}>
+            {/* <div className="img-container">
                 <img alt={props.name} src={props.image} />
-            </div>
+            </div> */}
             <div className="content">
               <ul>
                 <li>
